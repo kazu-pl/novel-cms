0ef6ee0360e45c117bcb7ebbcd59619bcbf90375
Kamil Żurad <kamil.zurad@gmail.com>
2021-12-05 20:35:23 +0100

    fix returning to prev page with queries in url after logout, disallow to select the same act as nextAct

--- src/features/act/components/ActForm/ActForm.tsx
+++ src/features/act/components/ActForm/ActForm.tsx
@@ -14,6 +14,12 @@ export interface ActFormProps {
   actDictionary: SingleActDictionaryObject[];
   onSubmit: (values: Act, helpers: FormikHelpers<Act>) => void;
   submitButtonLabel: string;
+  /**
+   * When you edit some act, you can pass its `id` to prop `disallowedActIdToSelectAsNextAct` to prevent showing that act in select with list of `nextAct` acts. It will allow you to prevent chosing act A* as next act of the same act A*. So it prevents infinite loop of acts.
+   *
+   * * - act A is just an example of any Act.
+   */
+  disallowedActIdToSelectAsNextAct?: string;
 }
 
 const ActForm = ({
@@ -21,6 +27,7 @@ const ActForm = ({
   actDictionary,
   onSubmit,
   submitButtonLabel,
+  disallowedActIdToSelectAsNextAct,
 }: ActFormProps) => {
   const yup = useLocalizedYup();
   const { t } = useTranslation();
@@ -75,11 +82,13 @@ const ActForm = ({
                 fullWidth
                 label={t("actsPages.add.actForm.type")}
               >
-                {(["start", "normal", "end"] as Act["type"][]).map((item) => (
-                  <MenuItem key={item} value={item}>
-                    {item}
-                  </MenuItem>
-                ))}
+                {(["start", "normal", "end"] as Act["type"][]).map(
+                  (item, index) => (
+                    <MenuItem key={index} value={item}>
+                      {item}
+                    </MenuItem>
+                  )
+                )}
               </TextFieldFormik>
             </Box>
 
@@ -93,11 +102,21 @@ const ActForm = ({
                   fullWidth
                   label={t("actsPages.add.actForm.nextAct")}
                 >
-                  {actDictionary.map((item) => (
-                    <MenuItem key={item.id} value={item.id}>
-                      {item.title}
-                    </MenuItem>
-                  ))}
+                  {disallowedActIdToSelectAsNextAct
+                    ? actDictionary
+                        .filter(
+                          ({ id }) => id !== disallowedActIdToSelectAsNextAct
+                        )
+                        .map((item) => (
+                          <MenuItem key={item.id} value={item.id}>
+                            {item.title}
+                          </MenuItem>
+                        ))
+                    : actDictionary.map((item) => (
+                        <MenuItem key={item.id} value={item.id}>
+                          {item.title}
+                        </MenuItem>
+                      ))}
                 </TextFieldFormik>
               </Box>
             )}
