867a172bedd3af7457dacd30098fff476ee3657f
Kamil Żurad <kamil.zurad@gmail.com>
2021-12-29 22:28:36 +0100

    use StyleSheetManager to disableVendorPrefixes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import {
   ThemeProvider as MuiThemeProvider,
   StyledEngineProvider,
 } from "@mui/material/styles";
-import { ThemeProvider as StyledThemeProvider } from "styled-components";
+import {
+  ThemeProvider as StyledThemeProvider,
+  StyleSheetManager,
+} from "styled-components";
 import CssBaseline from "@mui/material/CssBaseline";
 import CreateGlobalStyle from "common/styles/globalStyles.styled";
 import { BrowserRouter } from "react-router-dom";
@@ -17,19 +20,21 @@ import muiTheme from "common/theme/muiTheme";
 function App() {
   return (
     <Suspense fallback={<CircularProgress size={80} />}>
-      <StyledEngineProvider injectFirst>
-        <MuiThemeProvider theme={muiTheme}>
-          <StyledThemeProvider theme={muiTheme}>
-            <CssBaseline />
-            <CreateGlobalStyle />
-            <SnackbarProvider>
-              <BrowserRouter>
-                <Router />
-              </BrowserRouter>
-            </SnackbarProvider>
-          </StyledThemeProvider>
-        </MuiThemeProvider>
-      </StyledEngineProvider>
+      <StyleSheetManager disableVendorPrefixes>
+        <StyledEngineProvider injectFirst>
+          <MuiThemeProvider theme={muiTheme}>
+            <StyledThemeProvider theme={muiTheme}>
+              <CssBaseline />
+              <CreateGlobalStyle />
+              <SnackbarProvider>
+                <BrowserRouter>
+                  <Router />
+                </BrowserRouter>
+              </SnackbarProvider>
+            </StyledThemeProvider>
+          </MuiThemeProvider>
+        </StyledEngineProvider>
+      </StyleSheetManager>
     </Suspense>
   );
 }
